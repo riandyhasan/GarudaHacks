a5e73304d9a57040d13ba3d5bdebcc9842d5a854
riandyhasan <riandyhsn@gmail.com>
2021-11-21 22:00:22 +0700

    style global theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+const theme = extendTheme({
+  fonts: {
+    heading: "Gilroy",
+    body: "Gilroy",
+  },
+  colors: {
+    brand: {
+      100: "#5A38FD",
+      200: "#64A1F4",
+      300: "#BFD6F6",
+    },
+  },
+});
+
+function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Global />
       <Component {...pageProps} />
     </ChakraProvider>
   );
 }
 
-export default MyApp;
+export default App;
