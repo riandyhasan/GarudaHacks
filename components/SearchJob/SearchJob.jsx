d607ab7f726167cc8afc69025906f036fde88720
riandyhasan <riandyhsn@gmail.com>
2021-11-21 22:55:02 +0700

    Update SearchJob.jsx

--- components/SearchJob/SearchJob.jsx
+++ components/SearchJob/SearchJob.jsx
@@ -4,9 +4,9 @@ import Filterbox from "../Filterbox/Filterbox";
 import Jobcard from "../JobCard/JobCard";
 const SearchJob = ({ ...props }) => {
   return (
-    <Flex {...props}>
+    <Flex {...props} paddingRight="1rem" paddingBottom="3rem">
       <Filterbox />
-      <Grid templateColumns="repeat(2, 350px)" gridGap="2rem">
+      <Grid templateColumns="repeat(2, 350px)" gridGap="2rem" marginLeft="2rem">
         <Jobcard />
         <Jobcard />
         <Jobcard />
