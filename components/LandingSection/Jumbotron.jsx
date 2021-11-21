9035f6dad870249c405ab6e916c015ec6d0b69a7
riandyhasan <riandyhsn@gmail.com>
2021-11-21 22:54:59 +0700

    Update Jumbotron.jsx

--- components/LandingSection/Jumbotron.jsx
+++ components/LandingSection/Jumbotron.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { Box, Text, Flex, Button, Heading } from "@chakra-ui/react";
 
 const Jumbotron = () => {
@@ -14,10 +15,15 @@ const Jumbotron = () => {
             Creating Equality.
           </Heading>
         </Heading>
-        <Text mt="2rem">Grid is a web app -- </Text>
-        <Button bg="brand.200" boxShadow="0px 4px 8px rgba(0, 0, 0, 0.25)" borderRadius="30px" color="white" _hover={{ color: "brand.200", bg: "white" }} mt="4rem">
-          Learn More
-        </Button>
+        <Text mt="2rem" alignText="justify" maxW="30vw">
+          SemuaSama is particularly inspired from an energetic and positive disabled individual that i met on jalan maliboro a while a go. He explained that he currently has no job due to the reason that he didn’t have a proper
+          accessibility towards a place or company that would accept him.{" "}
+        </Text>
+        <Link href="#aboutus">
+          <Button bg="brand.200" boxShadow="0px 4px 8px rgba(0, 0, 0, 0.25)" borderRadius="30px" color="white" _hover={{ color: "brand.200", bg: "white" }} mt="4rem">
+            Learn More
+          </Button>
+        </Link>
       </Box>
       <Image src="/assets/images/landing.png" width={750} height={550} />
     </Flex>
